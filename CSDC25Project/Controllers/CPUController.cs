c3d0fc55628f2f06866c370545ea06da1b8ca70b
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-04-06 21:48:05 +0800

    Add project files.

---
+++ CSDC25Project/Controllers/CPUController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSDC25Project.Controllers
+{
+    public class CPUController : Controller
+    {
+        // GET: CPU
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        public ActionResult Teacher_Dashboard()
+        {
+            return View();
+        }
+        public ActionResult Student_Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult AdminDashboard()
+        {
+            return View();
+        }
+
+        public ActionResult FileMaintenance()
+        {
+            return View();
+        }
+    }
+}
---
+++ CSDC25Project/Models/LoginCredentials.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSDC25Project.Models
+{
+    public class LoginCredentials
+    {
+
+        public string User_primary_id { get; set; }
+        public int Role_id { get; set; }
+        public string User_name { get; set; }
+        public string User_Password { get; set; }
+        public int Login_Attempts { get; set; }
+    }
+}
